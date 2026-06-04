afd5cafe1fdeabd9864f66a33fbee1c19d15fbdb
davidbsu <davidfromasd@gmail.com>
2026-06-04 13:31:52 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'polar-align-v1';
+ 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.add('./'))
+      .then(() => self.skipWaiting())
+  );
+});
+ 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+ 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(resp => {
+        if (resp && resp.status === 200 && resp.type === 'basic') {
+          caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+        }
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
+ 
